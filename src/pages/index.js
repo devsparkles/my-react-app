ba5c04cac35ca5af274b980ca5cc9fdc1ddba4d8
Xavier Maximin <xavier.maximin.uk@gmail.com>
2018-09-16 17:15:32 +0100

    add all file

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to Design and Code Android Application</h1>
+    <p>Complete course about the best tools and design. Prototype and build apps with Android and Kotlin.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
