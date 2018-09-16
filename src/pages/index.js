6817009b79be59a1ec3c9d2a285c45000d186769
Xavier Maximin <xavier.maximin.uk@gmail.com>
2018-09-17 00:13:08 +0100

    first hero design

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to Design and Code Android Application</h1>
-    <p>Complete course about the best tools and design. Prototype and build apps with Android and Kotlin.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to Design and Code Android Application</h1>
+        <p>Complete course about the best tools and design. Prototype and build apps with Android and Kotlin.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+
   </div>
 )
 
